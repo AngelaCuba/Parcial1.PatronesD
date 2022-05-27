803e5b53d954f308917e15343627390207655762
Anyi Cuba <83591073+AngelaCuba@users.noreply.github.com>
2022-05-27 08:22:21 -0400

    Parcial 1  commmit 2

--- src/main/java/singlretoP/Vehiculo.java
+++ src/main/java/singlretoP/Vehiculo.java
@@ -4,4 +4,19 @@ public class Vehiculo {
     private int placa;
     private String modelo;
     private String color;
+    private String tipo;
+
+    public Vehiculo (int placa, String modelo, String color, String tipo){
+        this.placa = placa;
+        this.modelo = modelo;
+        this.color = color;
+        this.tipo = tipo;
+    }
+    public String getTipo(){
+        return tipo;
+    }
+
+    public void setTipo(String Tipo) {
+        this.tipo = tipo;
+    }
 }
